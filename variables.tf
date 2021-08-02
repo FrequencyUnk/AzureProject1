8127009b4770e89b98c73d994d39ea6cd08148f2
FrequencyUnk <77994041+FrequencyUnk@users.noreply.github.com>
2021-08-02 16:24:06 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,11 @@
 variable "sub" {
   description = "This is the subscription id for _____ project."
 }
+
+variable "name" {
+  description = "This is the name for _____ Resource Group."
+}
+
+variable "location" {
+  description = "This is the location for _____ Resource Group."
+}
